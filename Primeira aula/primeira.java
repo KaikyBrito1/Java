7eda2a6cc8cad773e20e7b1aba6bdf1d1e50b5c4
KaikyBrito1 <okaiiky2019@gmail.com>
2026-08-03 22:01:17 -0300

    Create primeira.java

---
+++ Primeira aula/primeira.java
@@ -0,0 +1,15 @@
+
+package com.poo.prj__poo1_aula270726;
+
+
+public class Prj__POO1_Aula270726 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Teste t = new Teste();
+        t.mostrar();
+        t.somar();
+        t.mostrarValor(t.somar("1", "4"));
+    }
+}
